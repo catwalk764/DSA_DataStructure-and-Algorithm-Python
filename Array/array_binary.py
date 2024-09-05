5bd461d9873c461da6687c048ad4730de00bca2b
Sathish <ctwk764@gmail.com>
2024-09-05 13:28:16 -0400

    adding more content

--- Array/array_binary.py
+++ Array/array_binary.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
-
-def binary(arr, target):
+#The function binary is actually implementing a binary search algorithm. 
+# It's designed to find the index of a target value in a sorted array.
+# First code finds the index of target value, second value of give index.
+'''def binary(arr, target):
     if len(arr) <= 1:
         return arr
     
@@ -9,7 +11,7 @@ def binary(arr, target):
     right = len(arr) - 1
 
     while left <= right:
-        mid = len(arr) // 2
+        mid = (left + right) // 2
         if arr[mid] == target:
             return mid
         elif arr[mid] < target:
@@ -21,4 +23,16 @@ def binary(arr, target):
 arr = [2, 36, 9, 65, 7, 4, 78, 5, 5, 7]
 arr.sort()
 print(arr)
-print("index of the element is :", binary(arr, 7))
+print("index of the element is :", binary(arr, 7))'''
+
+def binary(arr, index):
+    left = 0
+    right = len(arr) -1
+
+    if 0 <= index < len(arr):
+        return arr[index]
+    return None
+arr = [2, 36, 9, 65, 7, 4, 78, 5, 5, 7]
+arr.sort()
+print(arr)
+print("value at index is:", binary (arr, 7))
